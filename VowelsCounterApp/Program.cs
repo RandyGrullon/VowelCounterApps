9706933a73a4f0d63f32032e6e6d965659d41b1b
Randy Grullon <54919808+RandyGrullon@users.noreply.github.com>
2021-09-16 20:29:46 -0400

    Subir Codigo

---
+++ VowelsCounterApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VowelsCounterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputTextToVowelCount();
+        }
+        public static void InputTextToVowelCount()
+        {
+
+            string input = string.Empty;
+            while (input != "N")
+            {
+                Console.WriteLine("Escriba un texto para contentar sus vocales.");
+                input = Console.ReadLine();
+                var vowels = Counter.VowelsCounter.Count(input);
+                if (vowels.Count == 0)
+                {
+                    Console.WriteLine($"Esta lista esta vacia su entrada fue ({input})");
+                }
+                else
+                {
+                    foreach (var item in vowels)
+                    {
+                        Console.WriteLine($"{{{item.Key}:{item.Value}}}\n");
+                    }
+                }
+                Console.WriteLine("Para Salir Digite la letra *(N)*");
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
